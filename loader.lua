24fab827f98446cb942ba076b2da4a0e0795eecc
piyushganguwarop <ganguwarpiyush@gmail.com>
2026-01-21 13:17:03 +0000

    bug fixes

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
 
 local games = {
-    [{124311897657957}] = "",
+    [{124311897657957}] = "https://raw.githubusercontent.com/piyushganguwarop/hub/refs/heads/main/Games/balb.lua",
 }
 
 for ids, url in next, games do
